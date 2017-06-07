1b688839043cdf505fdc44fb3021388bd3982328
Axel <axel.erlich@yahoo.com.ar>
2017-06-07 00:53:29 -0300

    Agrega tests para algunos casos de uso

--- DragonBall/src/Tablero.java
+++ DragonBall/src/Tablero.java
@@ -4,6 +4,12 @@ public class Tablero {
 	
 	public Tablero(int pTamanio){
 		this.celdas = new ArrayList<Celda>();
+		
+		for(int i = 0; i < pTamanio; i++){
+			for(int j = 0; j<pTamanio; j++){
+				
+			}
+		}
 	}
 	
 	private ArrayList<Celda> celdas;
