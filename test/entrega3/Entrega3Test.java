d502cf2b00e148ed184c4927c980db9c45aa71ca
Axel Brian Erlich <AErlich@sofrecom.com.ar>
2017-06-15 11:41:51 -0300

    Agrega tests para entrega 3

--- test/entrega3/Entrega3Test.java
+++ test/entrega3/Entrega3Test.java
@@ -19,7 +19,22 @@ public class Entrega3Test {
 	
 	/*
 	@Test
-	public void test2ConsumirSemilla(){
+	public void test2SemillaDesapareceAlPisar(){
+		Tablero tablero = new Tablero(10);
+		SemilladelHermitano semilla = new SemilladelHermitano(new Coordenada(3,3));
+		Goku goku = new Goku(new Coordenada(1,1));
+		
+		tablero.agregarUbicable(goku);
+		tablero.agregarUbicable(semilla);
+		
+		goku.Mover(new Coordenada(3,3));
+		tablero.actualizarPosiciones();
+
+		org.junit.Assert.assertEquals(goku.getVida(), 600);
+	}
+	
+	@Test
+	public void test3ConsumirSemilla(){
 		Tablero tablero = new Tablero(10);
 		SemilladelHermitano semilla = new SemilladelHermitano(new Coordenada(3,3));
 		Goku goku = new Goku(new Coordenada(1,1));
@@ -31,6 +46,43 @@ public class Entrega3Test {
 		tablero.actualizarPosiciones();
 
 		org.junit.Assert.assertEquals(semilla, null);
-	}*/
+	}
+
+	@Test
+	public void test3ConsumirEsferaDragon(){
+		Tablero tablero = new Tablero(10);
+		EsferaDelDragon esfera = new EsferaDelDragon(new Coordenada(3,3));
+		Goku goku = new Goku(new Coordenada(1,1));
+		Freezer freezer = new Freezer(new Coordenada(2,2));
+		
+		tablero.agregarUbicable(goku);
+		tablero.agregarUbicable(esfera);
+		
+		goku.Mover(new Coordenada(3,3));
+		tablero.actualizarPosiciones();
+
+		goku.atacar(freezer);
+
+		org.junit.Assert.assertEquals(freezer.getVida(), 375);
+	}
+	
+	@Test
+	public void test3ConsumirNubeVoladora(){
+		Tablero tablero = new Tablero(10);
+		NubeVoladora nube = new NubeVoladora(new Coordenada(3,3));
+		Goku goku = new Goku(new Coordenada(1,1));
+		
+		tablero.agregarUbicable(goku);
+		tablero.agregarUbicable(nube);
+		
+		goku.Mover(new Coordenada(3,3));
+		tablero.actualizarPosiciones();
+
+		goku.Mover(new Coordenada(7,7));
+
+		org.junit.Assert.assertEquals(goku.getUbicacion(), new Coordenada(7,7));
+	}
+	
+	*/
 	
 }
